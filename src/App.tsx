5643cbc8f0e0cb42d3ceee01963c9d6c611f6b7a
joyonto <engr.joyontoroy@gmail.com>
2023-01-23 23:32:26 +0600

    typescript setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="App">
+      <h3>Joyonto Kumar Roy</h3>
+    </div>
+  );
+};
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,9 @@ import App from "./App";
 import "./assets/styles/main.scss";
 
 console.log("get react dom", ReactDOM);
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
 console.log("root retun value", root);
 root.render(
   <React.StrictMode>
